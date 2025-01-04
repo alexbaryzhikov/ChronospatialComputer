a0cf6e5a907eb37bf7323c4ae52cc98ad561ecd9
Aleksei Baryzhikov <aleksiarts@gmail.com>
2025-01-04 19:10:46 +0100

    Update vector-sum

--- vector-sum/VectorSum.cpp
+++ vector-sum/VectorSum.cpp
@@ -1,4 +1,5 @@
 #include <cassert>
+#include <cxxabi.h>
 #include <simd/simd.h>
 
 #define NS_PRIVATE_IMPLEMENTATION
@@ -7,58 +8,62 @@
 #include <VectorIO.hpp>
 #include <Print.hpp>
 
-struct ComputationHandler {
-    MTL::Device* device;
-    MTL::CommandQueue* commandQueue;
-    MTL::ComputePipelineState* computePipelineState;
-    MTL::Buffer* bufferA;
-    MTL::Buffer* bufferB;
-    MTL::Buffer* bufferResult;
+template<typename T>
+std::unique_ptr<char, void(*)(void*)> typeName() {
+    int status = 0;
+    const char* mangled_name = typeid(T).name();
+    char* demangled_name = abi::__cxa_demangle(mangled_name, nullptr, nullptr, &status);
+    return std::unique_ptr<char, void(*)(void*)>(demangled_name, std::free);
+}
 
-    void init() {
-        device = MTL::CreateSystemDefaultDevice();
-        commandQueue = device->newCommandQueue();
-        buildComputePipelineState();
+template<typename T>
+std::shared_ptr<T> autoRelease(T* resource, NS::Error** error = nullptr) {
+    if (!resource) {
+        print("Couldn't create ", typeName<T>().get());
+        if (error) {
+            println(": ", (*error)->localizedDescription()->utf8String());
+        } else {
+            println();
+        }
+        assert(false);
     }
-    
-    void release() {
-        computePipelineState->release();
-        commandQueue->release();
-        device->release();
-    }
+    println("Created ", typeName<T>().get(), " at ", resource);
+    return std::shared_ptr<T>(resource, [](T* resource) {
+        resource->release();
+        println("Released ", typeName<T>().get(), " at ", resource);
+    });
+}
 
-    void buildComputePipelineState() {
+struct GPUHandler {
+    std::shared_ptr<MTL::Device> device;
+    std::shared_ptr<MTL::CommandQueue> commandQueue;
+    std::shared_ptr<MTL::ComputePipelineState> computePipelineState;
+
+    GPUHandler()
+    : device(autoRelease(MTL::CreateSystemDefaultDevice()))
+    , commandQueue(autoRelease(device->newCommandQueue())) {
         NS::Error* error = nullptr;
-        MTL::Library* vectorSumLib = device->newDefaultLibrary();
-        if (!vectorSumLib) {
-            println(error->localizedDescription()->utf8String());
-            assert(false);
-        }
-        MTL::Function* addVectorsFn = vectorSumLib->newFunction(NS::String::string("addVectors", NS::UTF8StringEncoding));
-        computePipelineState = device->newComputePipelineState(addVectorsFn, &error);
-        if (!computePipelineState) {
-            println(error->localizedDescription()->utf8String());
-            assert(false);
-        }
-        addVectorsFn->release();
-        vectorSumLib->release();
+        auto vectorSumLib = autoRelease(device->newDefaultLibrary());
+        auto addVectorsFn = autoRelease(vectorSumLib->newFunction(NS::String::string("addVectors", NS::UTF8StringEncoding)));
+        computePipelineState = autoRelease(device->newComputePipelineState(addVectorsFn.get(), &error), &error);
     }
     
     std::vector<float> vectorSum(const std::vector<float>& a, const std::vector<float>& b) {
+        auto pool = NS::AutoreleasePool::alloc()->init();
+
         size_t vectorSize = a.size();
         size_t bufferSize = vectorSize * sizeof(float);
+        auto bufferA = device->newBuffer(a.data(), bufferSize, MTL::ResourceStorageModeShared);
+        auto bufferB = device->newBuffer(b.data(), bufferSize, MTL::ResourceStorageModeShared);
+        auto bufferResult = device->newBuffer(bufferSize, MTL::ResourceStorageModeShared);
 
-        bufferA = device->newBuffer(a.data(), bufferSize, MTL::ResourceStorageModeShared);
-        bufferB = device->newBuffer(b.data(), bufferSize, MTL::ResourceStorageModeShared);
-        bufferResult = device->newBuffer(bufferSize, MTL::ResourceStorageModeShared);
-
-        MTL::CommandBuffer* commandBuffer = commandQueue->commandBuffer();
-        assert(commandBuffer);
-        MTL::ComputeCommandEncoder* commandEncoder = commandBuffer->computeCommandEncoder();
-        commandEncoder->setComputePipelineState(computePipelineState);
+        auto commandBuffer = commandQueue->commandBuffer();
+        auto commandEncoder = commandBuffer->computeCommandEncoder();
+        commandEncoder->setComputePipelineState(computePipelineState.get());
         commandEncoder->setBuffer(bufferA, 0, 0);
         commandEncoder->setBuffer(bufferB, 0, 1);
         commandEncoder->setBuffer(bufferResult, 0, 2);
+
         MTL::Size threadsPerGrid(vectorSize, 1, 1);
         MTL::Size threadsPerThreadgroup(computePipelineState->maxTotalThreadsPerThreadgroup(), 1, 1);
         commandEncoder->dispatchThreads(threadsPerGrid, threadsPerThreadgroup);
@@ -69,21 +74,23 @@ struct ComputationHandler {
         std::vector<float> result(vectorSize);
         std::memcpy(result.data(), bufferResult->contents(), bufferResult->length());
 
-        bufferA->release();
-        bufferB->release();
-        bufferResult->release();
+        pool->release();
 
         return result;
     }
 };
 
-int main(int argc, const char* argv[]) {
-    NS::AutoreleasePool* pAutoreleasePool = NS::AutoreleasePool::alloc()->init();
-    ComputationHandler handler;
-    handler.init();
-    auto result = handler.vectorSum({0, 1, 2, 3, 4}, {5, 6, 7, 8, 9});
+void compute() {
+    GPUHandler gpuHandler;
+    std::vector<float> a = {rand() / float(RAND_MAX), rand() / float(RAND_MAX), rand() / float(RAND_MAX)};
+    std::vector<float> b = {rand() / float(RAND_MAX), rand() / float(RAND_MAX), rand() / float(RAND_MAX)};
+    std::vector<float> result = gpuHandler.vectorSum(a, b);
     println("Result: ", result);
-    handler.release();
-    pAutoreleasePool->release();
+}
+
+int main(int argc, const char* argv[]) {
+    auto pool = NS::AutoreleasePool::alloc()->init();
+    compute();
+    pool->release();
     return 0;
 }
